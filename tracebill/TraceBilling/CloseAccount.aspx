854804f05a29da9c838efde3178b5e37efa53d06
NGOBI DANIEL <ngobizadokchrist@gmail.com>
2021-05-31 15:16:42 +0300

    adding close account module

---
+++ tracebill/TraceBilling/CloseAccount.aspx
@@ -0,0 +1,210 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/General.Master" AutoEventWireup="true" CodeBehind="CloseAccount.aspx.cs" Inherits="TraceBilling.CloseAccount" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <div class="container">
+	<div class="row">
+	  <form role="form" runat="server">
+          <br />
+          <div><h3>VIEW APPLICATIONS INITIATED</h3></div>
+    
+          <center>
+                <asp:Label runat="server" ID="lblmsg" Visible="false" ></asp:Label>
+            </center>
+
+        <br />
+        <div class="form-group col-sm-12">
+          <%--  <asp:MultiView ID="MultiView1" runat="server">
+        <asp:View ID="View1" runat="server">--%>
+                          <table width="100%">
+    <tr>
+    <%--<th>New</th>--%>
+           <th class="modal-sm" style="width: 250px">Application Name</th>
+        <th class="modal-sm" style="width: 236px">Country</th>
+         <th class="datepicker-inline" style="width: 226px">Operation Area</th>
+        <th></th>
+        </tr>
+        <tr>
+      <%--  <td>
+        
+        <asp:ImageButton ID="ImageButtonedit"  ImageAlign="AbsMiddle"
+         AlternateText="search" runat="server" ImageUrl="assets/dist/img/add.png" 
+         CssClass="btn-default inline" Width="50" Height="40" OnClick="ImageButton1_Click" />
+        </td>--%>
+           <td class="modal-sm" style="width: 250px">
+                           <asp:TextBox ID="txtapplicationname" runat="server" CssClass="form-control" Width="217px" ></asp:TextBox>
+                     
+                          </td> 
+                          <td class="modal-sm" style="width: 236px" >
+                      
+            <asp:DropDownList ID="country_list" CssClass="form-control" runat="server"  OnDataBound="country_list_DataBound" Visible="true" AutoPostBack="True"
+                 OnSelectedIndexChanged="country_list_SelectedIndexChanged" Width="235px">
+                </asp:DropDownList>
+                    </td>
+             
+                           
+             <td class="datepicker-inline" style="width: 226px" >
+                      
+            <asp:DropDownList ID="area_list" CssClass="form-control" runat="server"  OnDataBound="area_list_DataBound" Visible="true" >
+                
+                </asp:DropDownList>
+                    </td>
+                
+                 
+                          <td>
+                          <asp:Button ID="Button3" Width="150" Height="40" CssClass="btn-primary"
+                                    runat="server" Text="Search" onclick="Button3_Click" />
+                      
+           
+                          </td>
+                                                 
+                          </tr>
+                          </table>
+                          </div>
+          <hr />
+          <div id="returnbtn" runat="server">
+              <asp:Button ID="btnreturn" Width="253px" Height="40px" CssClass="btn-primary" Visible="false"
+                                    runat="server" Text="Return to application view list" onclick="btnReturn_Click" />
+          </div>
+            <div class="col-sm-12 home card" id="maindisplay" runat="server" >
+
+                   <h5 class="inline">View application Logs</h5>
+             
+              
+                             <asp:GridView ID="gv_applicationview" runat="server" 
+                       CssClass="grid-text" CellPadding="10" 
+                              ForeColor="#333333" GridLines="None" Width="100%"
+                                  AutoGenerateColumns="False" 
+                                 OnRowCommand="gv_applicationview_RowCommand"
+                                  OnRowDataBound="gv_applicationview_RowDataBound"   
+                                 onselectedindexchanging="gv_applicationview_SelectedIndexChanging"
+                                  onselectedindexchanged="gv_applicationview_SelectedIndexChanged"                                                             
+                                 >
+             <Columns>
+               
+           <asp:BoundField DataField="No" HeaderText="No" NullDisplayText="-"/> 
+             <asp:BoundField DataField="applicationNumber" HeaderText="Application#" NullDisplayText="-"/> 
+               <asp:BoundField DataField="ApplicationID" HeaderText="ApplicationID" NullDisplayText="-" Visible="false"/> 
+
+             <asp:BoundField DataField="fullName" HeaderText="Name" NullDisplayText="-" /> 
+             <asp:BoundField DataField="address" HeaderText="Address" NullDisplayText="-" /> 
+            
+            <%-- <asp:BoundField DataField="contact" HeaderText="Contact" NullDisplayText="-" /> --%>
+                
+                
+                 <asp:BoundField DataField="statusName" HeaderText="Status" NullDisplayText="-" /> 
+             <asp:BoundField DataField="typeName" HeaderText="CustomerType" NullDisplayText="-">
+              
+                 </asp:BoundField> 
+   <asp:BoundField DataField="className" HeaderText="className" NullDisplayText="-" /> 
+                 <%-- <asp:BoundField DataField="serviceName" HeaderText="serviceName" NullDisplayText="-" /> --%>
+               <asp:BoundField DataField="countryName" HeaderText="Country" NullDisplayText="-" /> 
+                  <asp:BoundField DataField="areaName" HeaderText="Area" NullDisplayText="-" /> 
+                  <asp:BoundField DataField="areaId" HeaderText="AreaID" NullDisplayText="-" Visible="false" />
+              <%--   <asp:BoundField DataField="countryId" HeaderText="CountryID" NullDisplayText="-"  Visible="false"/> 
+                  --%>
+                 <asp:BoundField DataField="assignedTo" HeaderText="AssignedTo" NullDisplayText="-" /> 
+             <%--    <asp:ButtonField ButtonType="Button" CommandName="btnPrint" HeaderText="Foam"
+            Text="Print" ItemStyle-ForeColor="Green" />
+                <asp:CommandField ShowSelectButton="true" ButtonType="Button" SelectText="View"   HeaderText="Details"/>--%>
+          <asp:TemplateField ShowHeader="True">
+                      <HeaderTemplate>
+                        Foam
+                    </HeaderTemplate>
+            <ItemTemplate>
+                <asp:LinkButton ID="PrintButton"
+                                runat="server"
+                                CommandName="RowPrint" 
+                     CommandArgument='<%#Eval("applicationNumber") + ";" +Eval("areaId") %>'
+                               
+                                Text="Print" />
+                 
+            </ItemTemplate>
+                     <ItemStyle Width="5%" />
+                 </asp:TemplateField>
+                       <asp:TemplateField ShowHeader="True">
+                      <HeaderTemplate>
+                        Logs
+                    </HeaderTemplate>
+            <ItemTemplate>
+                <asp:LinkButton ID="viewButton"
+                                runat="server"
+                                CommandName="RowView" 
+                    CommandArgument='<%#Eval("applicationNumber") %>'
+                               
+                                Text="View" />
+                 
+            </ItemTemplate>
+                     <ItemStyle Width="5%" />
+                 </asp:TemplateField>          
+             </Columns>
+             
+             <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+             <EditRowStyle BackColor="#2461BF" />
+             <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" Font-Underline="false" ForeColor="#333333" />
+             <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+             <AlternatingRowStyle BackColor="White" CssClass="GridRows" HorizontalAlign="Left" />
+             <RowStyle BackColor="#EFF3FB" CssClass="GridRows" HorizontalAlign="Left" />
+             <HeaderStyle CssClass="GridTopHeaderCell" Font-Bold="True" BackColor="#3c8dbc" ForeColor="White" />
+                <SortedAscendingCellStyle BackColor="#F5F7FB" />
+                <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+                <SortedDescendingCellStyle BackColor="#E9EBEF" />
+                <SortedDescendingHeaderStyle BackColor="#4870BE" />
+             </asp:GridView>
+
+               
+           <%-- </asp:View>
+                </asp:MultiView>--%>
+                      
+             
+            </div>
+          <br />
+        <div class="form-group col-sm-12 col-md-12 col-lg-12">
+                  <%--<center>--%>
+               <div id="statuslogdisplay" style="width:800px; margin:0 auto;  text-align: center;"  runat="server" visible="false">
+                <asp:Label runat="server" Text="View Application status logs" id="lbllogmsg" Visible="true" Font-Bold="true" ForeColor="Blue"></asp:Label>
+               <asp:GridView ID="gvlogdisplay" runat="server" 
+                       CssClass="grid-text" CellPadding="5" 
+                              ForeColor="#333333" GridLines="None" Width="92%"
+                                  AutoGenerateColumns="False"
+                              
+                   >
+             <Columns>                
+           <asp:BoundField DataField="No" HeaderText="No." NullDisplayText="-"/> 
+                <asp:BoundField DataField="applicationNumber" HeaderText="application#" NullDisplayText="-" />   
+             <asp:BoundField DataField="statusName" HeaderText="Status"  NullDisplayText="-"/>                
+             <asp:BoundField DataField="LoggedBy" HeaderText="Logged By" NullDisplayText="-"/> 
+             <asp:BoundField DataField="LogDate" HeaderText="log Date" NullDisplayText="-" />                         
+
+        
+              
+             </Columns>
+             
+             <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+             <EditRowStyle BackColor="#2461BF" />
+             <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" Font-Underline="false" ForeColor="#333333" />
+             <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+             <AlternatingRowStyle BackColor="White" CssClass="GridRows" HorizontalAlign="Left" />
+             <RowStyle BackColor="#EFF3FB" CssClass="GridRows" HorizontalAlign="Left" />
+             <HeaderStyle CssClass="GridTopHeaderCell" Font-Bold="True" BackColor="#3c8dbc" ForeColor="White" />
+                <SortedAscendingCellStyle BackColor="#F5F7FB" />
+                <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+                <SortedDescendingCellStyle BackColor="#E9EBEF" />
+                <SortedDescendingHeaderStyle BackColor="#4870BE" />
+             </asp:GridView>
+               </div>
+          <%--</center>--%>
+            <br />
+            <center>
+               <asp:Button ID="btnreturn3" runat="server" Text="Return" cssclass ="btn-primary" OnClick="btnreturn3_Click" />
+              &nbsp;&nbsp;&nbsp;&nbsp;
+               
+          </center>
+                  </div>
+        
+    </form>
+
+    <br /><br />
+        
+	</div>
+ 
+</div>
+</asp:Content>
---
+++ tracebill/TraceBilling/CloseAccount.aspx.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using TraceBilling.ControlObjects;
+using TraceBilling.EntityObjects;
+using System.Drawing;
+
+namespace TraceBilling
+{
+    public partial class CloseAccount : System.Web.UI.Page
+    {
+        public DataTable dt;
+        BusinessLogic bll = new BusinessLogic();
+        ApplicationObj app = new ApplicationObj();
+        ResponseMessage resp = new ResponseMessage();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (IsPostBack == false)
+                {
+
+                    LoadCountryList();
+                    int countryid = Convert.ToInt16(country_list.SelectedValue.ToString());
+                    LoadAreaList(countryid);
+                    LoadApplicationByStatus();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        private void LoadCountryList()
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                dt = bll.GetCountryList();
+                country_list.DataSource = dt;
+
+                country_list.DataTextField = "countryName";
+                country_list.DataValueField = "countryId";
+                country_list.DataBind();
+            }
+            catch (Exception ex)
+            {
+                string error = "100: " + ex.Message;
+                bll.Log("DisplayCountryList", error);
+                DisplayMessage(error, true);
+            }
+        }
+        private void LoadAreaList(int countryid)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                dt = bll.GetAreaList(countryid);
+                area_list.DataSource = dt;
+
+                area_list.DataTextField = "areaName";
+                area_list.DataValueField = "areaId";
+                area_list.DataBind();
+            }
+            catch (Exception ex)
+            {
+                string error = "100: " + ex.Message;
+                bll.Log("DisplayAreaList", error);
+                DisplayMessage(error, true);
+            }
+        }
+
+        private void DisplayMessage(string message, Boolean isError)
+        {
+            lblmsg.Visible = true;
+            lblmsg.Text = "MESSAGE: " + message + ".";
+            if (isError == true)
+            {
+                lblmsg.ForeColor = System.Drawing.Color.Red;
+            }
+            else
+            {
+                lblmsg.ForeColor = System.Drawing.Color.Green;
+            }
+        }
+        protected void country_list_DataBound(object sender, EventArgs e)
+        {
+            country_list.Items.Insert(0, new ListItem("- - select country - -", "0"));
+        }
+        protected void area_list_DataBound(object sender, EventArgs e)
+        {
+            area_list.Items.Insert(0, new ListItem("- - select area - -", "0"));
+        }
+
+        protected void country_list_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                //int deptid = int.Parse(department_list.SelectedValue.ToString());
+                int countryid = Convert.ToInt16(country_list.SelectedValue.ToString());
+                LoadAreaList(countryid);
+                //load session data
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                LoadApplicationByStatus();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void LoadApplicationByStatus()
+        {
+            try
+            {
+                string applicationame = txtapplicationname.Text.Trim();
+                string country = country_list.SelectedValue.ToString();
+                string area = area_list.SelectedValue.ToString();
+                string status = "0";
+                DataTable dataTable = bll.GetApplicationByStatus(applicationame, country, area, status);
+                if (dataTable.Rows.Count > 0)
+                {
+                    gv_applicationview.DataSource = dataTable;
+                    gv_applicationview.DataBind();
+                    DisplayMessage(".", true);
+                    maindisplay.Visible = true;
+                }
+                else
+                {
+                    string error = "100: " + "No records found";
+                    bll.Log("GetApplication", error);
+                    DisplayMessage(error, true);
+                    maindisplay.Visible = false;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        protected void btnReturn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                maindisplay.Visible = true;
+                btnreturn.Visible = true;
+                LoadApplicationByStatus();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        //protected void gv_applicationview_OnRowCommand(object sender, GridViewCommandEventArgs e)
+        //{
+        //    //Get the Command Name.
+        //    string commandName = e.CommandName;
+
+        //    if (commandName == "btnPrint")//routecard
+        //    {
+        //        string str = "Sorry, Application Foam print out not available yet!!!";
+        //        DisplayMessage(str, true);
+        //    }
+
+        //}
+
+
+
+        //protected void gv_applicationview_RowDataBound(object sender, GridViewRowEventArgs e)
+        //{
+        //    /*if (e.Row.RowType == DataControlRowType.DataRow)
+        //    {
+        //        //
+        //        // dispatchdisplay.Visible = true;
+        //        TableCell link = (TableCell)e.Row.Cells[1];
+        //        string status = e.Row.Cells[1].Text;
+        //        if ((status.Equals("Emergency")))
+        //        {
+
+        //            link.ForeColor = Color.Red;
+        //            //e.Row.BackColor = Color.Red;
+        //            //e.Row.ForeColor = Color.White;
+        //        }
+        //        else if ((status.Equals("General")))
+        //        {
+
+        //            link.ForeColor = Color.Green;
+        //            // e.Row.BackColor = Color.Green;
+        //            // e.Row.ForeColor = Color.White;
+        //        }
+        //        else if ((status.Equals("Others")))
+        //        {
+
+        //            link.ForeColor = Color.Blue;
+        //            // e.Row.BackColor = Color.Blue;
+        //            //e.Row.ForeColor = Color.White;
+        //        }
+        //    }*/
+        //}
+        protected void gv_applicationview_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                //
+                // dispatchdisplay.Visible = true;
+                TableCell link = (TableCell)e.Row.Cells[2];
+                string type = e.Row.Cells[6].Text;
+                // e.Row.BackColor = Color.Blue;
+                e.Row.ForeColor = Color.Green;
+
+            }
+        }
+        protected void gv_applicationview_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        protected void gv_applicationview_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+
+            if (e.CommandName == "RowPrint")
+            {
+                string str = "";
+                //string UserID = e.Item.Cells[0].Text;
+                //string appid = Convert.ToString(e.CommandArgument.ToString());
+                //string str = "Sorry, Application Foam print out not available yet!!!";
+                string[] arg = new string[2];
+                arg = e.CommandArgument.ToString().Split(';');
+                string appnumber = arg[0];
+                string areaid = arg[1];
+                PrintFoam(appnumber, areaid);
+                //DisplayMessage(str, true);
+            }
+            else if (e.CommandName == "RowView")
+            {
+                string appid = Convert.ToString(e.CommandArgument.ToString());
+                //string str = "Sorry, Application Foam print out not available yet!!!";
+                // DisplayMessage(returned, true);
+                LoadApplicationStatusLogs(appid);
+            }
+        }
+        public void PrintFoam(string appnumber, string areaid)
+        {
+            try
+            {
+                PDFPrints pp = new PDFPrints();
+                //string referenceno = "26012021/234/210/0/1";//1:application no, 2:paymentref, 3:referenceno
+                string flag = "1";
+                string res = "";
+                //string companyid = "2";
+                string user = Session["FullName"].ToString();
+                DataTable dt = bll.GetCustomerReportData(appnumber, flag);
+                DataTable dtprofile = bll.GetCompanyProfile(areaid);
+                if (dt.Rows.Count > 0)
+                {
+                    res = pp.GetPDFForm(dt, dtprofile, user);
+                    //Console.WriteLine(res);
+                    DisplayMessage(res, true);
+                }
+                else
+                {
+                    res = "Sorry, Application Foam print out not available yet!!!";
+                    DisplayMessage(res, true);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        protected void gv_applicationview_SelectedIndexChanging(object sender, GridViewSelectEventArgs e)
+        {
+            int index = e.NewSelectedIndex;
+            if (index >= 0)
+            {
+                //string refnumber = GridViewIssue.Rows[index].Cells[0].Text;
+                string usercode = gv_applicationview.Rows[index].Cells[1].Text;
+
+
+            }
+        }
+        private void LoadApplicationStatusLogs(string appnumber)
+        {
+            DataTable dt = new DataTable();
+            statuslogdisplay.Visible = true;
+            try
+            {
+                dt = bll.GetApplicationTrackLogs(appnumber);
+                gvlogdisplay.DataSource = dt;
+                //gvMaterial.CurrentPageIndex = 0;
+                gvlogdisplay.DataBind();
+                gvlogdisplay.Visible = true;
+            }
+            catch (Exception ex)
+            {
+                string error = "100: " + ex.Message;
+                bll.Log("LoadStatusLogDisplay", error);
+                DisplayMessage(error, true);
+            }
+        }
+
+        protected void btnreturn2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                maindisplay.Visible = true;
+                btnreturn.Visible = true;
+                LoadApplicationByStatus();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        protected void btnreturn3_Click(object sender, EventArgs e)
+        {
+            maindisplay.Visible = true;
+            statuslogdisplay.Visible = false;
+            LoadApplicationByStatus();
+        }
+    }
+}
